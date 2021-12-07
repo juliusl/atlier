59672fb1a3e13ffda8a566f6674b1ff7b759eb13
Julius Liu <juliusl@microsoft.com>
2021-12-07 01:19:55 -0800

    WIP: Putting the pieces together
    Signed-off-by: Julius Liu <juliusl@microsoft.com>

--- src/modules/editor.rs
+++ src/modules/editor.rs
@@ -1,7 +1,12 @@
-use specs::{System, Entities};
 
-use crate::prelude::{NodeInterior, NodeVisitor, State, NodeResource};
+use std::collections::BTreeSet;
+use specs::Component as SpecsComponent;
+use specs::{Entities, Join, ReadStorage, System, VecStorage};
 
+use crate::{
+    prelude::{NodeInterior, NodeResource, NodeVisitor, State},
+    store::Store,
+};
 
 struct Editor;
 
@@ -14,20 +19,8 @@ struct Component(State);
 
 impl Component {
     fn input(&self, param: fn() -> &'static str) -> Self {
-
         self.call(NodeResource::Input(param, None))
     }
-}
-
-#[test]
-fn test_xor_link() {
-    let a: u64 = 10;
-    let b: u64 = 5; 
-
-    let link = a ^ b;
-
-    assert_eq!(link^a, b);
-    assert_eq!(link^b, a);
 }
 
 impl<'a> NodeVisitor<'a> for Component {
@@ -52,8 +45,79 @@ impl<'a> System<'a> for Component {
     type SystemData = Entities<'a>;
 
     fn run(&mut self, data: Self::SystemData) {
-
         // Should this be the thing rendering?
         todo!()
+    }
+}
+
+#[derive(Clone)]
+struct EditorNode {
+    config: Store<&'static str>,
+}
+
+impl Default for EditorNode {
+    fn default() -> Self {
+        Self {
+            config: Store::default()
+                .node("component")
+                .node("input")
+                .node("output"),
+        }
+    }
+}
+
+impl<'a> NodeVisitor<'a> for EditorNode {
+    type Parameters = NodeComponent;
+
+    fn call(&self, params: Self::Parameters) -> Self {
+        let mut config = self.config.clone();
+        match params {
+            NodeComponent::Input(..) => config = config.link("component", "input"),
+            NodeComponent::Output(..) => config = config.link("component", "output"),
+        };
+
+        Self {
+            config
+        }
+    }
+
+    fn evaluate(&self) -> Option<State> {
+        todo!()
+    }
+}
+
+#[derive(Clone)]
+enum NodeComponent {
+    Input(String),
+    Output(String),
+}
+
+impl SpecsComponent for NodeComponent {
+    type Storage = VecStorage<Self>;
+}
+
+impl<'a> System<'a> for EditorNode {
+    type SystemData = (Entities<'a>, ReadStorage<'a, NodeComponent>);
+
+    fn run(&mut self, data: Self::SystemData) {
+        let mut seen: BTreeSet<&'static str> = BTreeSet::new();
+        let mut visited: BTreeSet<(Option<&'static str>, Option<&'static str>)> = BTreeSet::new();
+
+        // loop through all components
+        for (e, component) in (&data.0, &data.1).join() {
+            let state = self.clone().call(component.clone());
+            seen.clear();
+            visited.clear();
+
+            state.config.walk_ordered("component", &mut seen, &mut visited);
+
+            if !visited.insert((Some("component"), Some("input"))) {
+                // todo
+            }
+
+            if !visited.insert((Some("component"), Some("output"))) {
+                // todo
+            }
+        }
     }
 }
