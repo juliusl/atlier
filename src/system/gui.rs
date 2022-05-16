db5fc2457851e68f82c4a72adcbe89b6b943d3af
Julius Liu <juliusl@microsoft.com>
2022-05-16 00:09:57 -0700

    Nit. Make clear color a bit darker

--- src/system/gui.rs
+++ src/system/gui.rs
@@ -171,7 +171,7 @@ where
                             ops: wgpu::Operations {
                                 load: wgpu::LoadOp::Clear(wgpu::Color {
                                     r: 0.1,
-                                    g: 0.4,
+                                    g: 0.2,
                                     b: 0.3,
                                     a: 1.0,
                                 }),
