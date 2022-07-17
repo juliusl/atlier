662ec847fd653a7db3bd0df646cad2235d42dc1e
Julius Liu <juliusl@microsoft.com>
2022-07-17 16:48:23 -0700

    adjust clear color

--- src/system/gui.rs
+++ src/system/gui.rs
@@ -192,9 +192,9 @@ where
                             resolve_target: None,
                             ops: wgpu::Operations {
                                 load: wgpu::LoadOp::Clear(wgpu::Color {
-                                    r: 0.157,
-                                    g: 0.173,
-                                    b: 0.204,
+                                    r: 0.1,
+                                    g: 0.2,
+                                    b: 0.3,
                                     a: 1.0,
                                 }),
                                 store: true,
